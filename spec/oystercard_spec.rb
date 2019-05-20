2a5ed092737c63d224c76af07b3443c8420deb55
Helen Hayward <helen.m.hayward@hotmail.co.uk>
2019-05-20 14:22:03 +0100

    Top up method created

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,9 @@ describe Oystercard do
   it '#balance == 0' do
     expect(subject.balance).to eq(0)
   end
+
+  it '#top_up(1000)' do
+    subject.top_up(1000)
+    expect(subject.balance).to eq(1000)
+  end
 end
