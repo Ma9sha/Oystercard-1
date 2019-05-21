609f714202b6c712eafa5c4929fe34e99ee83ed3
Manisha Patel <ma9shap.gmail.com>
2019-05-21 15:42:13 +0100

    Added entry and exit station validation on journey class

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,9 +2,22 @@ require 'journey'
 describe Journey do
   let(:journey) {Journey.new}
   let(:bank_dbl) {double("station", name: "Bank")}
-  it 'exiting a station' do
+  let(:baker_street_dbl) {double("station", name: "Baker Street")}
+
+  it 'exiting a station without entry station set' do
     journey.exit_station = bank_dbl
     expect(journey.calculate_cost).to eq(600)
+  end
+
+  it "entering a station not finishing journey" do
+    journey.entry_station = baker_street_dbl
+    expect(journey.calculate_cost).to eq(600)
+  end
+
+  it "completed journey" do 
+    journey.exit_station = bank_dbl
+    journey.entry_station = baker_street_dbl
+    expect(journey.calculate_cost).to eq(100)
   end
   
 end
