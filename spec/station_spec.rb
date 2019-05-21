5eee2f168ed412b8a47e3646ee4a9ed0ae5a6241
Manisha Patel <ma9shap.gmail.com>
2019-05-21 14:47:29 +0100

    created new station class

---
+++ spec/station_spec.rb
@@ -0,0 +1,15 @@
+require 'station'
+
+describe Station do
+
+  it 'station name' do
+    station = Station.new("Stratford", 1)
+    expect(station.name).to eq("Stratford")
+  end
+
+  it 'station name' do
+    station = Station.new("Stratford", 1)
+    expect(station.zone).to eq(1)
+  end
+
+end
